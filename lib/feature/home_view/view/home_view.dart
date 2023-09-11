95286887644ab42a8bbbd50caa02c03f06ec7734
sayanths <sayanthsayu619@email.com>
2023-09-11 22:55:38 +0530

    complete responsive done

--- lib/feature/home_view/view/home_view.dart
+++ lib/feature/home_view/view/home_view.dart
@@ -27,120 +27,137 @@ class HomeView extends StatelessWidget {
           Consumer<HomeController>(
             builder: (context, userList, _) {
               return kIsWeb && ResponsiveUi.isDesktop(context)
-                  ? Row(
-                      children: [
-                        Expanded(
-                          child: CustomContainer(
-                            height: MediaQuery.of(context).size.height,
-                            width: MediaQuery.of(context).size.width,
-                            color: Apc.red,
-                          ),
-                        ),
-                        Expanded(
-                          child: CustomContainer(
-                            height: MediaQuery.of(context).size.height,
-                            width: MediaQuery.of(context).size.width,
-                            child: ListView.builder(
-                              shrinkWrap: true,
-                              itemCount: userList.myPostList?.length ?? 0,
-                              itemBuilder: (context, index) {
-                                final data = userList.myPostList?[index];
-                                return SizedBox(
-                                  height: 150,
-                                  child: Card(
-                                    elevation: 3,
-                                    child: Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Expanded(
-                                            child: CachedNetworkImage(
-                                          imageUrl: data?.image ?? "",
-                                          height: 150,
-                                          fit: BoxFit.cover,
-                                          errorWidget: (context, url, error) {
-                                            return const Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.center,
-                                              children: [
-                                                CupertinoActivityIndicator(
-                                                  color: Apc.grey,
-                                                ), // Show Cupertino activity indicator on error
-                                                SizedBox(height: 10),
-                                                Text('Error loading image'),
-                                              ],
-                                            );
-                                          },
-                                        )),
-                                        Expanded(
-                                            flex: 2,
-                                            child: Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.start,
-                                              children: [
-                                                UserDetailShowingWidget(
-                                                  subTitleSize: Responsive
-                                                          .textMultiplier! *
-                                                      1,
-                                                  titleSize: Responsive
-                                                          .textMultiplier! *
-                                                      1,
-                                                  size: 5,
-                                                  title: 'Name',
-                                                  subTitle:
-                                                      data?.username ?? "",
-                                                ),
-                                                UserDetailShowingWidget(
-                                                  subTitleSize: Responsive
-                                                          .textMultiplier! *
-                                                      1,
-                                                  titleSize: Responsive
-                                                          .textMultiplier! *
-                                                      1,
-                                                  size: 5,
-                                                  title: 'Age',
-                                                  subTitle:
-                                                      data?.age.toString() ??
-                                                          "",
-                                                ),
-                                                UserDetailShowingWidget(
-                                                  maxLines: 2,
-                                                  overflow:
-                                                      TextOverflow.ellipsis,
-                                                  subTitleSize: Responsive
-                                                          .textMultiplier! *
-                                                      1,
-                                                  titleSize: Responsive
-                                                          .textMultiplier! *
-                                                      1,
-                                                  size: 5,
-                                                  title: 'Address',
-                                                  subTitle:
-                                                      "${data?.address?.address} , ${data?.address?.city}",
-                                                ),
-                                                UserDetailShowingWidget(
-                                                  subTitleSize: Responsive
-                                                          .textMultiplier! *
-                                                      1,
-                                                  titleSize: Responsive
-                                                          .textMultiplier! *
-                                                      1,
-                                                  size: 5,
-                                                  title: 'Company',
-                                                  subTitle:
-                                                      data?.company?.name ?? "",
-                                                ),
-                                              ],
-                                            ))
-                                      ],
+                  ? Wrap(
+                      children: List.generate(userList.myPostList?.length ?? 0,
+                          (index) {
+                        final data = userList.myPostList?[index];
+                        return GestureDetector(
+                          onTap: () async {
+                            context
+                                .read<OverViewController>()
+                                .getUserIndex(index + 1);
+                            await context
+                                .read<OverViewController>()
+                                .getAllOverViewObject();
+                          },
+                          child: SizedBox(
+                            height: 150,
+                            width: Responsive.widthMultiplier! * 60,
+                            child: Card(
+                              elevation: 3,
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Expanded(
+                                      child: CustomContainer(
+                                    height: 120,
+                                    width: 50,
+                                    decoration: BoxDecoration(
+                                        color: const Color.fromARGB(
+                                            255, 0, 162, 146),
+                                        borderRadius: BorderRadius.circular(6)),
+                                    child: CachedNetworkImage(
+                                      imageUrl: data?.image ?? "",
+                                      height: 150,
+                                      fit: BoxFit.cover,
+                                      errorWidget: (context, url, error) {
+                                        return const Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            CupertinoActivityIndicator(
+                                              color: Apc.grey,
+                                            ), // Show Cupertino activity indicator on error
+                                            SizedBox(height: 10),
+                                            Text('Error loading image'),
+                                          ],
+                                        );
+                                      },
                                     ),
-                                  ),
-                                );
-                              },
+                                  )),
+                                  Expanded(
+                                      flex: 2,
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          UserDetailShowingWidget(
+                                            subTitleSize:
+                                                Responsive.textMultiplier! * 1,
+                                            titleSize:
+                                                Responsive.textMultiplier! * 1,
+                                            size: 5,
+                                            title: 'Name',
+                                            subTitle: data?.username ?? "",
+                                          ),
+                                          UserDetailShowingWidget(
+                                            subTitleSize:
+                                                Responsive.textMultiplier! * 1,
+                                            titleSize:
+                                                Responsive.textMultiplier! * 1,
+                                            size: 5,
+                                            title: 'Age',
+                                            subTitle:
+                                                data?.age.toString() ?? "",
+                                          ),
+                                          UserDetailShowingWidget(
+                                            maxLines: 1,
+                                            overflow: TextOverflow.ellipsis,
+                                            subTitleSize:
+                                                Responsive.textMultiplier! * 1,
+                                            titleSize:
+                                                Responsive.textMultiplier! * 1,
+                                            size: 5,
+                                            title: 'Address',
+                                            subTitle:
+                                                "${data?.address?.address} , ${data?.address?.city}",
+                                          ),
+                                          UserDetailShowingWidget(
+                                            subTitleSize:
+                                                Responsive.textMultiplier! * 1,
+                                            titleSize:
+                                                Responsive.textMultiplier! * 1,
+                                            size: 5,
+                                            title: 'Company',
+                                            subTitle: data?.company?.name ?? "",
+                                          ),
+                                        ],
+                                      ))
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      ],
+                        );
+                      }),
                     )
+                  // ? Row(
+                  //     children: [
+                  //       Expanded(
+                  //         child: CustomContainer(
+                  //           height: MediaQuery.of(context).size.height,
+                  //           width: MediaQuery.of(context).size.width,
+                  //           color: Apc.white,
+                  //         ),
+                  //       ),
+                  //       Expanded(
+                  //         child: CustomContainer(
+                  //           height: MediaQuery.of(context).size.height,
+                  //           width: MediaQuery.of(context).size.width,
+                  //           child: List.generate(, (index) {
+
+                  //           })
+                  //           // ListView.builder(
+                  //           //   shrinkWrap: true,
+                  //           //   itemCount: userList.myPostList?.length ?? 0,
+                  //           //   itemBuilder: (context, index) {
+                  //           //     final data = userList.myPostList?[index];
+                  //           //     return
+                  //           //   },
+                  //           // ),
+                  //         ),
+                  //       ),
+                  //     ],
+                  //   )
                   : ListView.builder(
                       shrinkWrap: true,
                       itemCount: userList.myPostList?.length ?? 0,
